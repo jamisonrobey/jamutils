7a2536e2ae190a85ed0a0ff9317c6947f99daaf6
jamisonrobey <jamisonrobey@gmail.com>
2025-08-11 11:20:48 +1000

    refactor: styling + use ifndef header guard

--- jamutils/FD.h
+++ jamutils/FD.h
@@ -1,56 +1,68 @@
-#pragma once
+#ifndef FD_H
+#define FD_H
 
 #include <fcntl.h>
 #include <filesystem>
 #include <stdexcept>
+#include <system_error>
 #include <unistd.h>
 #include <utility>
 
+namespace jam_utils
+{
+class FD
+{
+  public:
+    explicit FD(int fd) : fd_{fd}
+    {
+        if (fd_ < 0)
+        {
+            throw std::invalid_argument("FD given negative descriptor");
+        }
+    }
 
-namespace jam_utils {
-class FD {
-public:
-  explicit FD(const int fd) : fd_{fd} {
-    if (fd_ < 0)
-      throw std::invalid_argument("FD(int) ctor given negative descriptor");
-  }
+    explicit FD(const std::filesystem::path& file_path, int flags = O_RDONLY) : fd_{open(file_path.c_str(), flags)}
+    {
+        if (fd_ < 0)
+        {
+            throw std::system_error(errno, std::system_category());
+        }
+    }
 
+    FD(const FD&) = delete;
+    FD& operator=(const FD&) = delete;
 
-  explicit FD(const std::filesystem::path& file_path,
-              const int flags = O_RDONLY) :
-    fd_{open(file_path.c_str(), flags)} {
-    if (fd_ < 0)
-      throw std::system_error(errno, std::system_category());
-  }
+    FD(FD&& other) noexcept : fd_{std::exchange(other.fd_, -1)} {}
+    FD& operator=(FD&& other) noexcept
+    {
+        if (this != &other)
+        {
+            if (fd_ >= 0)
+            {
+                close(fd_);
+            }
+            fd_ = std::exchange(other.fd_, -1);
+        }
+        return *this;
+    }
 
-  FD(const FD&) = delete;
-  FD& operator=(const FD&) = delete;
+    ~FD()
+    {
+        if (fd_ >= 0)
+        {
+            close(fd_);
+        }
+    }
 
-  FD(FD&& other) noexcept : fd_{std::exchange(other.fd_, -1)} {}
+    [[nodiscard]]
+    int fd() const noexcept
+    {
+        return fd_;
+    }
 
-  FD& operator=(FD&& other) noexcept {
-    if (this == &other)
-      return *this;
-
-    if (fd_ >= 0)
-      close(fd_);
-
-    fd_ = std::exchange(other.fd_, -1);
-
-    return *this;
-  }
-
-  ~FD() {
-    if (fd_ >= 0)
-      close(fd_);
-  }
-
-  [[nodiscard]]
-  int fd() const noexcept {
-    return fd_;
-  }
-
-private:
-  int fd_{-1};
+  private:
+    int fd_{-1};
 };
 }
+
+#endif
--- jamutils/M_Map.h
+++ jamutils/M_Map.h
@@ -1,81 +1,91 @@
-#pragma once
-
-#include <sys/mman.h>
-#include <unistd.h>
-#include <system_error>
+#ifndef M_MAP_H
+#define M_MAP_H
 
 #include "FD.h"
 
-namespace jam_utils {
-class M_Map {
-public:
-  M_Map(const size_t length,
-        const int prot,
-        const int flags,
-        FD&& fd,
-        const off_t offset) :
-    fd_{std::move(fd)},
-    addr_{mmap(nullptr, length, prot, flags, fd_.fd(), offset)},
-    len_{length} {
-    if (addr_ == MAP_FAILED)
-      throw std::system_error(errno, std::system_category(), "mmap");
-  }
+#include <span>
+#include <sys/mman.h>
+#include <system_error>
+#include <unistd.h>
+#include <utility>
 
-  M_Map(const M_Map&) = delete;
-  M_Map& operator=(const M_Map&) = delete;
-
-  M_Map(M_Map&& other) noexcept : fd_{std::move(other.fd_)},
-                                  addr_{std::exchange(other.addr_, nullptr)},
-                                  len_{other.len_} {}
-
-
-  M_Map& operator=(M_Map&& other) noexcept {
-    if (this == &other)
-      return *this;
-
-    if (addr_) {
-      munmap(addr_, len_);
+namespace jam_utils
+{
+class M_Map
+{
+  public:
+    M_Map(size_t length, int prot, int flags, FD&& fd, off_t offset)
+        : fd_{std::move(fd)}, addr_{mmap(nullptr, length, prot, flags, fd_.fd(), offset)}, len_{length}
+    {
+        if (addr_ == MAP_FAILED)
+        {
+            throw std::system_error(errno, std::system_category());
+        }
     }
 
-    fd_ = std::move(other.fd_);
-    addr_ = std::exchange(other.addr_, nullptr);
-    len_ = other.len_;
+    M_Map(const M_Map&) = delete;
+    M_Map& operator=(const M_Map&) = delete;
 
-    return *this;
-  }
-
-  ~M_Map() {
-    if (addr_) {
-      munmap(addr_, len_);
+    M_Map(M_Map&& other) noexcept
+        : fd_{std::move(other.fd_)}, addr_{std::exchange(other.addr_, nullptr)}, len_{other.len_}
+    {
     }
-  }
 
-  [[nodiscard]]
-  int fd() const noexcept {
-    return fd_.fd();
-  }
+    M_Map& operator=(M_Map&& other) noexcept
+    {
+        if (this != &other)
+        {
+            if (addr_)
+            {
+                munmap(addr_, len_);
+            }
 
-  template <typename T = void>
-  [[nodiscard]]
-  T* addr() const noexcept {
-    return static_cast<T*>(addr_);
-  }
+            fd_ = std::move(other.fd_);
+            addr_ = std::exchange(other.addr_, nullptr);
+            len_ = other.len_;
+        }
+        return *this;
+    }
 
-  template <typename T>
-  [[nodiscard]]
-  std::span<T> as_span() const noexcept {
-    return {static_cast<T*>(addr_), len_ / sizeof(T)};
-  }
+    ~M_Map()
+    {
+        if (addr_)
+        {
+            munmap(addr_, len_);
+        }
+    }
 
+    [[nodiscard]]
+    int fd() const noexcept
+    {
+        return fd_.fd();
+    }
 
-  [[nodiscard]]
-  size_t len() const noexcept {
-    return len_;
-  }
+    template <typename T = void>
+    [[nodiscard]]
+    T* addr() const noexcept
+    {
+        return static_cast<T*>(addr_);
+    }
 
-private:
-  FD fd_;
-  void* addr_{nullptr};
-  size_t len_{};
+    template <typename T>
+    [[nodiscard]]
+    std::span<T> as_span() const noexcept
+    {
+        return {static_cast<T*>(addr_), len_ / sizeof(T)};
+    }
+
+    [[nodiscard]]
+    size_t len() const noexcept
+    {
+        return len_;
+    }
+
+  private:
+    FD fd_;
+    void* addr_;
+    size_t len_;
 };
 }
+
+#endif
